a6e2d38da43ddd31717d90c628d28e89f7ee6bf7
Joshua Trimble <jtrimble6@yahoo.com>
2018-09-04 23:40:43 -0500

    live on heroku

--- server.js
+++ server.js
@@ -57,6 +57,10 @@ app.use( (req, res, next) => {
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/supperclub");
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
