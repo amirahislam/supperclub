2a4fad114f7424f9be2fb3590c3024b126a11564
Joshua Trimble <jtrimble6@yahoo.com>
2018-09-05 19:45:26 -0500

    working on heroku

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(routes, buzzRoutes, eventRoutes, sessionRoutes);
 app.use(
   session({
     secret: 'fraggle-rock',
-    store: new MongoStore({ mongooseConnection: dbConnection }),
+    // store: new MongoStore({ mongooseConnection: dbConnection }),
     resave: false,
     saveUninitialized: false
   })
